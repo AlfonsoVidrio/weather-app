d0cfb11a5c643dd3a96103e40a34b013e2073018
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2026-07-17 18:33:21 -0600

    fix: use HTTPS for OpenWeatherMap API requests

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -41,7 +41,7 @@ export const useWeather = () => {
         setNotFound(false)
 
         try {
-            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
             const { data } = await axios.get(geoUrl)
 
